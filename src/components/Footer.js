2d5504272d5078c79dd8e6d707d4e92cc8b38d2d
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-02-08 08:37:01 +0100

    Create semantic components

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+const Footer = () => {
+    return(
+        <footer>
+            <img src="#" alt="Little Lemon Logo" />
+            <div>
+                <ul>
+                    <li>Doormat Navigation</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Contact</li>
+                    <li><a href="#">Address</a></li>
+                    <li><a href="#">Phone number</a></li>
+                    <li><a href="#">Email</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Social media</li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Linkedin</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <header>
+            <img src="#" alt="Little Lemon Logo"/>
+        </header>
+    )
+}
+
+export default Header
